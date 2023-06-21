2e10dcf377ad14f1c5476b07b2de2f6e7b5a6741
ECN Guest005 <ecng005@potr262lnx35.ecn.purdue.edu>
2023-06-21 09:34:54 -0400

    work was done

---
+++ source/program_counter_high.sv
@@ -0,0 +1,12 @@
+module programCH (
+    input logic [7:0] ADH_in,
+    input logic nrst, clk,
+    input logic PCH_PCH,
+    input logic ADH_PCH,
+    input logic PCLC,
+    input logic PCH_DB,
+    input logic PCH_ADH,
+    output logic [7:0] DB_out, ADH_out  //
+);
+
+endmodule
