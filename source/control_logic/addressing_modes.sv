066aba60026b36bb159bde177fe9a2b7d87d3d9d
TRMunson7 <munson8@purdue.edu>
2023-06-21 08:31:28 -0400

    always_comb begin     if(state == A0)begin         //Increment position         flags[PC_INC] = 1;         flags[LOAD_ABH] = 1;         flags[LOAD_ABL] = 1;         flags[SET_ADH_TO_PCH] = 1;         flags[SET_ADL_TO_PCL] = 1;         //Add data to X         flags[ALU_ADD] = 1;         flags[SET_DB_TO_DATA] = 1;         flags[SET_INPUT_B_TO_DB] = 1;         flags[SET_SB_TO_X] = 1;         flags[SET_INPUT_A_TO_SB] = 1;     end else if(state == A1)begin         //Increment position         flags[SET_ADH_TO_PCH] = 1;         flags[SET_ADL_TO_PCL] = 1;         //Move ALU output to ABL         flags[SET_ADL_TO_ALU] = 1;         flags[LOAD_ABL] = 1;         //Add data to X         flags[ALU_ADD] = 1;         flags[SET_DB_TO_DATA] = 1;         flags[SET_INPUT_B_TO_DB] = 1;         flags[SET_SB_TO_X] = 1;         flags[SET_INPUT_A_TO_SB] = 1;     end else if(state == A2)begin         //Move ALU output to ADL         flags[SET_SB_TO_ALU] = 1;         flags[SET_ADH_TO_SB] = 1;         flags[LOAD_ABH] = 1;     end else begin         flags = 0;     end end endmodule

--- source/control_logic/addressing_modes.sv
+++ source/control_logic/addressing_modes.sv
@@ -51,7 +51,7 @@ module ZPG
 );
 
 always_comb begin
-    if(state == 4'b0001)begin
+    if(state == A0)begin
         //go to zero page
         flags[SET_ADH_LOW] = 1;
         flags[SET_ADL_TO_DATA] = 1;
@@ -176,6 +176,45 @@ always_comb begin
         flags[SET_SB_TO_ALU] = 1;
         flags[SET_ADH_TO_SB] = 1;
         flags[LOAD_ABH] = 1;
+    end else begin
+        flags = 0;
+    end
+end
+endmodule
+
+module ZPG_X
+(
+    input logic [3:0] state,
+    input logic [7:0] opCode,
+    output logic [NUMFLAGS:0] flags
+);
+
+always_comb begin
+    if(state == 4'b0001)begin
+        //go to zero page
+        flags[SET_ADH_LOW] = 1;
+        flags[SET_ADL_TO_DATA] = 1;
+        flags[LOAD_ABH] = 1;
+        flags[LOAD_ABL] = 1;
+    end else begin
+        flags = 0;
+    end
+end
+
+always_comb begin
+    if(state == A0)begin
+        //Add data to X
+        flags[ALU_ADD] = 1;
+        flags[SET_DB_TO_DATA] = 1;
+        flags[SET_INPUT_B_TO_DB] = 1;
+        flags[SET_SB_TO_X] = 1;
+        flags[SET_INPUT_A_TO_SB] = 1;
+    end else if(state == A1)begin
+        //Set Zero Page
+        flags[SET_ADH_LOW] = 1;
+        flags[SET_ADL_TO_ALU] = 1;
+        flags[LOAD_ABH] = 1;
+        flags[LOAD_ABL] = 1;
     end else begin
         flags = 0;
     end
