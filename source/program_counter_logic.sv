0ff3d9dfc58d71b65daf3e458504ab50af42ac44
ECN Guest005 <ecng005@potr262lnx35.ecn.purdue.edu>
2023-06-21 14:47:53 -0400

    first attempt at PC logic

---
+++ source/program_counter_logic.sv
@@ -0,0 +1,16 @@
+module programCounterLogic (
+    input logic [7:0] input_lowbyte, input_highbyte,        //components of 16-bit address to be given to PC
+    input logic increment, decrement,                       //control flags for modiflying address
+    output logic [7:0] output_lowbyte, output_highbyte      //address output of the PC
+);
+
+    logic [15:0] address = {input_highbyte, input_lowbyte}; //inputs are operated on as a whole
+    always_comb begin
+        if(increment) address = address + 1;
+        else if(decrement) address = address - 1;
+        else address = address;
+    end
+
+    assign output_lowbyte = address[7:0];
+    assign output_highbyte = address[15:8];
+endmodule
