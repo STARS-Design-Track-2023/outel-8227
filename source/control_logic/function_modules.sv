e5584b0fc5e07b7e99375c0d00d624b928920735
TRMunson7 <munson8@purdue.edu>
2023-06-26 14:53:10 -0400

    update rti and start rts

--- source/control_logic/function_modules.sv
+++ source/control_logic/function_modules.sv
@@ -2361,12 +2361,11 @@ always_comb begin
             flags[LOAD_ALU] = 1;
         end
         T1: begin
-            //ALU to SP
-            flags[SET_SB_TO_ALU] = 1;
-            flags[LOAD_SP] = 1;
+            //ABL to ALU
+            flags[SET_ADL_TO_ALU] = 1;
+            flags[LOAD_ABL]  = 1;
 
-            //Set input B to SP
-            flags[SET_ADL_TO_SP] = 1;
+            //Set input B to ADL(SP+1)
             flags[SET_INPUT_B_TO_ADL] = 1;
 
             //Set A to 0
@@ -2378,12 +2377,11 @@ always_comb begin
             flags[LOAD_ALU] = 1;
         end
         T2: begin
-            //ALU to SP
-            flags[SET_SB_TO_ALU] = 1;
-            flags[LOAD_SP] = 1;
-            
-            //Set input B to SP
-            flags[SET_ADL_TO_SP] = 1;
+            ///ABL to ALU
+            flags[SET_ADL_TO_ALU] = 1;
+            flags[LOAD_ABL]  = 1;
+
+            //Set input B to ADL(SP+1)
             flags[SET_INPUT_B_TO_ADL] = 1;
 
             //Set A to 0
@@ -2400,6 +2398,10 @@ always_comb begin
             
         end
         T3: begin
+            ///ABL to ALU
+            flags[SET_ADL_TO_ALU] = 1;
+            flags[LOAD_ABL]  = 1;
+
             //ALU to SP
             flags[SET_SB_TO_ALU] = 1;
             flags[LOAD_SP] = 1;
@@ -2522,6 +2524,94 @@ always_comb begin
         end
         T3: begin
             
+        end
+        default: flags = 0;
+    endcase
+end
+    
+endmodule
+
+
+module RTS(
+    input logic [2:0] state,
+    output logic [NUMFLAGS-1:0] flags
+);
+
+always_comb begin
+    flags = 0;
+    case (state)
+        T0: begin
+            //Set input B to SP
+            flags[SET_SB_TO_SP] = 1;
+            flags[SET_DB_TO_SB] = 1;
+            flags[SET_INPUT_B_TO_DB] = 1;
+
+            //Set A to 0
+            flags[SET_INPUT_A_TO_LOW] = 1;
+
+            //Add 1 to SP
+            flags[SET_ALU_CARRY_HIGH] = 1;
+            flags[ALU_ADD] = 1;
+            flags[LOAD_ALU] = 1;
+        end
+        T1: begin
+            //ABL to ALU
+            flags[SET_ADL_TO_ALU] = 1;
+            flags[LOAD_ABL]  = 1;
+
+            //Set input B to ADL(SP+1)
+            flags[SET_INPUT_B_TO_ADL] = 1;
+
+            //Set A to 0
+            flags[SET_INPUT_A_TO_LOW] = 1;
+
+            //Add 1 to SP
+            flags[SET_ALU_CARRY_HIGH] = 1;
+            flags[ALU_ADD] = 1;
+            flags[LOAD_ALU] = 1;
+        end
+        T2: begin
+            //ABL to ALU
+            flags[SET_ADL_TO_ALU] = 1;
+            flags[LOAD_ABL]  = 1;
+
+            //SP to ALU
+            flags[SET_SB_TO_ALU] = 1;
+            flags[LOAD_SP] = 1;
+            
+            //Set input B to Data
+            flags[SET_DB_TO_DATA] = 1;
+            flags[SET_INPUT_B_TO_DB] = 1;
+
+            //Set A to 0
+            flags[SET_INPUT_A_TO_LOW] = 1;
+
+            //Save Data in ALU
+            flags[ALU_ADD] = 1;
+            flags[LOAD_ALU] = 1;
+        end
+        T3: begin
+            //Update ABL
+            flags[SET_ADL_TO_ALU] = 1;
+            flags[LOAD_ABL] = 1;
+
+            //Update ABH
+            flags[SET_ADH_TO_DATA] = 1;
+            flags[LOAD_ABH] = 1;
+
+            //Update PC
+            flags[LOAD_PC] = 1;
+            flags[PC_INC] = 1;
+        end
+        T4: begin
+            //Increment PC
+            flags[PC_INC] = 1;
+
+            //set ABH and ABL to PC
+            flags[SET_ADH_TO_PCH] = 1;
+            flags[LOAD_ABH] = 1;
+            flags[SET_ADL_TO_PCL] = 1;
+            flags[LOAD_ABL] = 1;
         end
         default: flags = 0;
     endcase
