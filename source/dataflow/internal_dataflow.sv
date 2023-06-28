10191e66783cf7c24520c60fe1906fd0766763db
TRMunson7 <munson8@purdue.edu>
2023-06-28 15:12:18 -0400

    Do flag and branch stuff

--- source/dataflow/internal_dataflow.sv
+++ source/dataflow/internal_dataflow.sv
@@ -250,7 +250,7 @@ module internalDataflow(
         .ldb_adl(flags[SET_INPUT_B_TO_ADL]),
         .lda_sb(flags[SET_INPUT_A_TO_SB]),
         .lda_zero(flags[SET_INPUT_A_TO_LOW]),
-        .enable_dec(flags[SET_ALU_DEC_TO_PSR_DEC]),
+        .enable_dec(flags[SET_ALU_DEC_TO_PSR_DEC] & psrRegToLogicController[3]),
         .carry_in(flags[SET_ALU_CARRY_HIGH] | (flags[SET_ALU_CARRY_TO_FREE_CARRY] & freeCarry) | (flags[SET_ALU_CARRY_TO_PSR_CARRY] & psrCarry)),
         .e_sum(flags[ALU_ADD]),
         .e_and(flags[ALU_AND]),
