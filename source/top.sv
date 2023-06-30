dc31268de62652a1e46d0aea18a29c60302c2865
Adrian <abuczko@purdue.edu>
2023-06-30 15:27:54 -0400

    lol

--- source/top.sv
+++ source/top.sv
@@ -18,6 +18,15 @@
 // `include "source/dataflow/program_counter_logic.sv"
 // `include "source/dataflow/register.sv"
 
+`default_nettype none
+`define RAM_MEMORY_MAP 16'b0000_000?_????_????
+`define ROM_MEMORY_MAP 16'b1111_111?_????_????
+
+`define RAM_CS 1
+`define ROM_CS 2
+
+`define ROM_FILE_NAME "source/program.mem"
+
 module top 
 (
   // I/O ports
@@ -54,7 +63,7 @@ module top
   //assign {ss7[0], ss6[0], ss5[0], ss4[0], ss3[0], ss2[0], ss1[0], ss0[0]} = dataBusIn;
 
   top8227 top8227 (
-    .clk(clk),
+    .clk(hwclk),
     .nrst(nrst),
     .nonMaskableInterrupt(nmi),
     .interruptRequest(irq),
@@ -151,7 +160,7 @@ module top
 
   fpga_seven_seg_driver fpga_seven_seg_driver
   (
-    .clk(clk),
+    .clk(hwclk),
     .nrst(nrst),
     .writeEnable(~rnw),
     .addressbusHigh(addressBusHigh), 
@@ -166,5 +175,89 @@ module top
     .ss6(ss6), 
     .ss7(ss7) 
   );
+
+  demo_mapped_io memory(
+    hwclk, 
+    {addressBusHigh, addressBusLow},
+    dataBusIn,
+    rnw,
+    dataBusOut
+  );
     
+endmodule
+
+
+
+module demo_mapped_io (
+    input logic clk,
+    input logic [15:0] addr,
+    input logic [7:0] din,
+    input logic read_en,
+    output logic [7:0] dout
+);
+
+  logic [1:0] cs; // Chip select
+  logic [7:0] ram_dout, rom_dout;
+
+  ram ram (
+      .clk(clk),
+      .din(din),
+      .addr(addr[8:0]),
+      .write_en(~read_en && (cs == `RAM_CS)),
+      .dout(ram_dout)
+  );
+
+  rom rom (
+      .clk(clk),
+      .data(rom_dout),
+      .addr(addr[8:0])
+  );
+
+  always_comb begin
+    casez (addr)
+      `RAM_MEMORY_MAP: cs = `RAM_CS;
+      `ROM_MEMORY_MAP: cs = `ROM_CS;
+      default: cs = 0;
+    endcase
+  end
+
+  always_comb begin
+    casez (cs)
+      `RAM_CS: dout = ram_dout;
+      `ROM_CS: dout = rom_dout;
+      default: dout = 0;
+    endcase
+  end
+    
+endmodule
+
+module ram (din, addr, write_en, clk, dout); // 512x8
+  parameter addr_width = 9;
+  parameter data_width = 8;
+  input [addr_width-1:0] addr;
+  input [data_width-1:0] din;
+  input write_en, clk;
+  output [data_width-1:0] dout;
+
+  reg [data_width-1:0] dout; // Register for output.
+  reg [data_width-1:0] mem [(1<<addr_width)-1:0];
+  always @(posedge clk)
+  begin
+    if (write_en)
+    mem[(addr)] <= din;
+    dout = mem[addr]; // Output register controlled by clock.
+  end
+endmodule
+
+module rom (
+  input clk,
+  input [8:0] addr,
+  output reg [7:0] data
+);
+    
+  reg [7:0] Rom [511:0];
+  initial $readmemh(`ROM_FILE_NAME, Rom);
+
+  always @(posedge clk)
+      data <= Rom[addr];
 endmodule
