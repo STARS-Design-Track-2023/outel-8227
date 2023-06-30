df6416c500dbf84087efd0eea73cfb7d6701c854
Adrian <abuczko@purdue.edu>
2023-06-30 15:35:42 -0400

    this branch is now shit

--- source/top.sv
+++ source/top.sv
@@ -80,82 +80,7 @@ module top
     .debugRed()
   );
 
-  //Memory Select (only 01 and 00 can be written to and overlap)
-  always_comb begin : memory_select
-    if(rnw)
-    begin
-      if(addressBusHigh > 8'H7F)
-      begin
-        dataBusIn = romOut;
-      end
-      else
-        dataBusIn = ramOut;//Note that all these pages overlap and writing will only affect pages 01 and 00
-    end
-    else
-    begin
-      dataBusIn = 8'H00;
-    end
-  end
-
-
-  //assign ramOut = 8'H00;
-  logic [127:0] ram, ramNext;
-  always_comb begin : RAM
-    ramNext = ram;
-    if(~rnw)
-    begin
-      //If writing, only write to the zero page and 1st page
-      if(addressBusHigh == 8'H00 || addressBusHigh == 8'H01)
-        ramNext[8*addressBusLow[3:0]+:8] = dataBusOut;
-    end
-  end
-
-  // If we are writing, the memory doesn't need to appear until the next clock cycle
-  always_ff @(posedge clk, negedge nrst) begin : RAM_next_state
-    if(~nrst)
-      ram <= 128'b0;
-    else
-      begin
-        ram <= ramNext;
-      end
-  end
-
-  //Set ramOut (01 and 00 pages will overlap)
-  assign ramOut = ram[8*addressBusLow[3:0]+:8] & {8{rnw}};//only set if reading
-
-  //Set RomOut
-  always_comb begin : RomOut
-    if(rnw)
-    begin
-      case({addressBusHigh, addressBusLow})
-        16'HFFFC: romOut = 8'H00;//ADL
-        16'HFFFD: romOut = 8'HFF;//ADH
-
-        16'HFF00: romOut = 8'HA2;
-        16'HFF01: romOut = 8'H00;
-        16'HFF02: romOut = 8'H8A;
-        16'HFF03: romOut = 8'HC9;
-        16'HFF04: romOut = 8'H08;
-        16'HFF05: romOut = 8'H10;
-        16'HFF06: romOut = 8'H06;
-        16'HFF07: romOut = 8'H8A;
-        16'HFF08: romOut = 8'H95;
-        16'HFF09: romOut = 8'H00;
-        16'HFF0A: romOut = 8'HE8;
-        16'HFF0B: romOut = 8'H4C;
-        16'HFF0C: romOut = 8'H03;
-        16'HFF0D: romOut = 8'HFF;
-        16'HFF0E: romOut = 8'H00;
-        16'HFF0F: romOut = 8'H00;
-
-        default:  romOut = 8'H00;
-      endcase
-    end
-    else
-    begin
-      romOut = 8'H00;
-    end
-  end
+  
 
   fpga_seven_seg_driver fpga_seven_seg_driver
   (
@@ -260,3 +185,80 @@ module rom (
   always @(posedge clk)
       data <= Rom[addr];
 endmodule
+
+// //Memory Select (only 01 and 00 can be written to and overlap)
+//   always_comb begin : memory_select
+//     if(rnw)
+//     begin
+//       if(addressBusHigh > 8'H7F)
+//       begin
+//         dataBusIn = romOut;
+//       end
+//       else
+//         dataBusIn = ramOut;//Note that all these pages overlap and writing will only affect pages 01 and 00
+//     end
+//     else
+//     begin
+//       dataBusIn = 8'H00;
+//     end
+//   end
+
+
+//   //assign ramOut = 8'H00;
+//   logic [127:0] ram, ramNext;
+//   always_comb begin : RAM
+//     ramNext = ram;
+//     if(~rnw)
+//     begin
+//       //If writing, only write to the zero page and 1st page
+//       if(addressBusHigh == 8'H00 || addressBusHigh == 8'H01)
+//         ramNext[8*addressBusLow[3:0]+:8] = dataBusOut;
+//     end
+//   end
+
+//   // If we are writing, the memory doesn't need to appear until the next clock cycle
+//   always_ff @(posedge clk, negedge nrst) begin : RAM_next_state
+//     if(~nrst)
+//       ram <= 128'b0;
+//     else
+//       begin
+//         ram <= ramNext;
+//       end
+//   end
+
+//   //Set ramOut (01 and 00 pages will overlap)
+//   assign ramOut = ram[8*addressBusLow[3:0]+:8] & {8{rnw}};//only set if reading
+
+//   //Set RomOut
+//   always_comb begin : RomOut
+//     if(rnw)
+//     begin
+//       case({addressBusHigh, addressBusLow})
+//         16'HFFFC: romOut = 8'H00;//ADL
+//         16'HFFFD: romOut = 8'HFF;//ADH
+
+//         16'HFF00: romOut = 8'HA2;
+//         16'HFF01: romOut = 8'H00;
+//         16'HFF02: romOut = 8'H8A;
+//         16'HFF03: romOut = 8'HC9;
+//         16'HFF04: romOut = 8'H08;
+//         16'HFF05: romOut = 8'H10;
+//         16'HFF06: romOut = 8'H06;
+//         16'HFF07: romOut = 8'H8A;
+//         16'HFF08: romOut = 8'H95;
+//         16'HFF09: romOut = 8'H00;
+//         16'HFF0A: romOut = 8'HE8;
+//         16'HFF0B: romOut = 8'H4C;
+//         16'HFF0C: romOut = 8'H03;
+//         16'HFF0D: romOut = 8'HFF;
+//         16'HFF0E: romOut = 8'H00;
+//         16'HFF0F: romOut = 8'H00;
+
+//         default:  romOut = 8'H00;
+//       endcase
+//     end
+//     else
+//     begin
+//       romOut = 8'H00;
+//     end
+//   end
