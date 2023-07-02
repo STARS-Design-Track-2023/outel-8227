9bffd7bd94fb12f2d5673a7e27326400d98f4b0b
AidanJacobsen <draidanfire@gmail.com>
2023-07-02 13:06:40 -0400

    Fixed STA ZPG, LDX ZPG

--- source/control_logic/decoder.sv
+++ source/control_logic/decoder.sv
@@ -190,11 +190,11 @@ module decoder (
                         end
                         3'b100: begin
                              cmd=`STX;
-                             address=`impl;
+                             address=`zpg;
                         end
                         3'b101: begin
                              cmd=`LDX;
-                             address=`impl;
+                             address=`zpg;
                         end
                         3'b110: begin
                              cmd=`DEC;
@@ -219,7 +219,7 @@ module decoder (
                     endcase
                 end
                 3'b011: begin
-                address=`abs;
+                    address=`abs;
                     case(a)
                         3'b000: cmd=`ASL;
                         3'b001: cmd=`ROL;
